a1fc9bfe628d8b34e6bb0956cd1af6f037496b83
Chun Yu <lamchunyu00@gmail.com>
2024-04-02 16:56:15 +0800

    Fixed download issue

--- self-hosting/assets/PresenterPrint-_QhK9_No.js
+++ self-hosting/assets/PresenterPrint-_QhK9_No.js
@@ -1,4 +1,4 @@
-import{d as _,u as d,a as h,c as m,b as p,r as u,o as a,e as n,f as t,t as s,g as r,F as f,h as g,n as v,i as x,j as y,k as b,l as k,m as N,_ as P}from"./index-sE4YQzMG.js";import{N as w}from"./NoteDisplay-rDsgs8jw.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
+import{d as _,u as d,a as h,c as m,b as p,r as u,o as a,e as n,f as t,t as s,g as r,F as f,h as g,n as v,i as x,j as y,k as b,l as k,m as N,_ as P}from"./index-FBCAqTGc.js";import{N as w}from"./NoteDisplay-P82O2UaF.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
 @page {
   size: A4;
   margin-top: 1.5cm;
